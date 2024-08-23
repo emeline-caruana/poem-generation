0449ebf029aac7b41af01b9800e39935b223a018
Ubuntu <ubuntu@ip-172-31-14-178.eu-west-1.compute.internal>
2024-08-23 11:51:52 +0000

    modified dockerfiles

--- application/front/script.js
+++ application/front/script.js
@@ -3,25 +3,29 @@ document.getElementById('apiForm').addEventListener('submit', function(event) {
 
     const topic = document.getElementById('data').value;
 
-    fetch('/predict', {  
+    // Use the backend service name defined in docker-compose.yaml
+    const backendUrl = 'http://backend:5050';
+
+    fetch(`${backendUrl}/predict`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ text: topic })  
+        body: JSON.stringify({ text: topic })
     })
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
-        
+
         var poemText = data.final_poem || 'No poem generated.';
 
-        var formattedData = poemText.replace(/\n/g, '<br>');  
+        var formattedData = poemText.replace(/\n/g, '<br>');
 
         document.getElementById('response').innerHTML = formattedData;
     })
     .catch((error) => {
         console.error('Error:', error);
-        document.getElementById('response').textContent = 'Erreur lors de l\'envoi des données.';
+        document.getElementById('response').textContent = 'Erreur lors de l\'envoi de la requête.';
     });
 });
+
